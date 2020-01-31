2d60387d4dd131c291b2820b4e6d2ab07e8c5f10
Your Name <you@example.com>
2020-01-31 04:47:36 +0100

    add access control

--- src/http/get-key/index.js
+++ src/http/get-key/index.js
@@ -2,6 +2,20 @@ let data = require('@begin/data')
 
 exports.handler = async function todos (req) {
   let body = req.queryStringParameters
+
+  if (body.secret != process.env.ACCESS_SECRET) {
+    return {
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json; charset=utf8',
+        'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      },
+      body: JSON.stringify({
+        "status": "access_denied"
+      })
+    }
+  }
+
   let keyValue = await data.get({
     table: 'keys',
     key: body.key
@@ -13,7 +27,7 @@ exports.handler = async function todos (req) {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     body: JSON.stringify({
-      keyValue
+      ...keyValue
     })
   }
 }
